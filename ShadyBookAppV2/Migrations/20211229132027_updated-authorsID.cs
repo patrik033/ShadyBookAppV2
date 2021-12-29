e620e77e5422f8ec359350f9044318107581880f
patrik033 <patrik.odh@gmail.com>
2021-12-29 14:34:45 +0100

    aa

---
+++ ShadyBookAppV2/Migrations/20211229132027_updated-authorsID.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShadyBookAppV2.Migrations
+{
+    public partial class updatedauthorsID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorsId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorsId",
+                table: "Books",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
